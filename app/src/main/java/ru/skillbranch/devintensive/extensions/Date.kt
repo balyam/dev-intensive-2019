98c8954a146aa32062d9cc1f000a0e93a57514b7
balyam <balyam@mail.ru>
2020-09-16 17:16:02 +0600

    fix m:d:h

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -12,13 +12,19 @@ fun Date.humanizeDiff(date: Date) : String {
     val now = Date().time
     var interval = now - date.time
     var diff = (interval/1000).toInt()
+    var dminute = diff/60
+    var dhour = diff/3600
+    var dday = diff/86400
     return when(diff){
         in 0..1  -> "только что"
         in 2..45 -> "несколько секунд назад"
         in 46..75 -> "минуту назад"
-        in 76..2700 -> "${diff/60} минут назад"
-        in 2701..4500 -> "${diff/60} минут назад"
-        in 4501..79200 -> "${diff/3600} час назад"
+        in 76..2700 -> if (dminute in 2..4) "$dminute минуты назад"  else "$dminute минут назад"
+        in 2701..4500 -> "$dhour час назад"
+        in 4501..79200 -> if (dhour in 2..4) "$dhour часа назад" else "$dhour часов назад"
+        in 79201..93600 -> "$dday день назад"
+        in 93601..31104000 -> if (dday in 2..4) "$dday дня назад" else "$dday дней назад"
+        it > 31104001 -> "более года назад"
         else -> throw  IllegalStateException("unknown time")
     }
 
