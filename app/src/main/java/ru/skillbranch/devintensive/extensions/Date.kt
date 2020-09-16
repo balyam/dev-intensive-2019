ebb69e65c7abdaac5f63e6ff5c09d0e602f67dde
Bolat <balyam@mail.ru>
2020-09-16 22:12:10 +0600

    fix Date extension

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,20 +1,17 @@
 package ru.skillbranch.devintensive.extensions
 
 import java.lang.IllegalStateException
-import java.text.SimpleDateFormat
-import java.time.Duration
-import java.time.format.DateTimeFormatter
 import java.util.*
-import java.util.regex.Pattern
+
 
 @Throws(IllegalStateException::class)
 fun Date.humanizeDiff(date: Date) : String {
     val now = Date().time
-    var interval = now - date.time
-    var diff = (interval/1000).toInt()
-    var dminute = diff/60
-    var dhour = diff/3600
-    var dday = diff/86400
+    val interval = now - date.time
+    val diff = (interval/1000).toInt()
+    val dminute = diff/60
+    val dhour = diff/3600
+    val dday = diff/86400
     return when(diff){
         in 0..1  -> "только что"
         in 2..45 -> "несколько секунд назад"
@@ -24,7 +21,7 @@ fun Date.humanizeDiff(date: Date) : String {
         in 4501..79200 -> if (dhour in 2..4) "$dhour часа назад" else "$dhour часов назад"
         in 79201..93600 -> "$dday день назад"
         in 93601..31104000 -> if (dday in 2..4) "$dday дня назад" else "$dday дней назад"
-        it > 31104001 -> "более года назад"
+        in 31104001..Integer.MAX_VALUE -> "более года назад"
         else -> throw  IllegalStateException("unknown time")
     }
 
