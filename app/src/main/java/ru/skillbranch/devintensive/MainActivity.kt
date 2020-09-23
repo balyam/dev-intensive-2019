4974dea42914fbe7576693f1aa682d14170ea4e1
balyam <balyam@mail.ru>
2020-09-23 17:17:23 +0600

    add method hideKeyboard

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -13,6 +13,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.extensions.hideKeyboard
 import ru.skillbranch.devintensive.models.Bender
 
 class MainActivity : AppCompatActivity(), View.OnClickListener  {
@@ -81,8 +82,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener  {
             val (r, g, b) = color
             benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
             textTxt.text= phrase
-
         }
+        this.hideKeyboard()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
