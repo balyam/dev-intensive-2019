173ceb780cbcffdffac5486c6818bcd55a87838e
balyam <balyam@mail.ru>
2020-10-09 17:21:39 +0600

    extend ProfileActivity

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -11,6 +11,7 @@ import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.extensions.hideKeyboard
@@ -18,16 +19,54 @@ import ru.skillbranch.devintensive.models.Bender
 
 class ProfileActivity : AppCompatActivity() {
 
+    companion object{
+        const val IS_EDIT_MODE = "IS_EDIT_MODE"
+    }
+
+    var isEditMode = false
+    lateinit var viewFields: Map<String, TextView>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        initViews(savedInstanceState)
 
     }
-
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+    }
 
+    private fun initViews(savedInstanceState: Bundle?) {
+        viewFields = mapOf(
+                "nickname" to tv_nick_name,
+                "rank" to tv_rank,
+                "rating" to tv_rating,
+                "respect" to tv_respect,
+                "firstname" to et_first_name,
+                "lastname" to et_last_name,
+                "repository" to et_repository,
+                "about" to et_about
+        )
+
+        btn_edit.setOnClickListener(View.OnClickListener {
+            isEditMode = !isEditMode
+            showCurrentMode(isEditMode)
+        })
+    }
+
+    private fun showCurrentMode(isEdit: Boolean) {
+        val info = viewFields.filter { setOf("firstname", "lastname", "repository", "about").contains(it.key) }
+
+        for ((_,v ) in info) {
+            v as TextInputEditText
+            v.isFocusable = isEdit
+            v.isFocusableInTouchMode = isEdit
+            v.isEnabled = isEdit
+            v.background.alpha = if (isEdit) 255 else 0
+        }
     }
 
 }
+
+
